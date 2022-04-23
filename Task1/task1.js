f283a5264243887924879b872906e904d532539a
q0abd <qiyomovabdulloh3@gmail.com>
2022-04-23 15:02:13 +0500

    Task1

--- Task1/task1.js
+++ Task1/task1.js
@@ -1,1 +1,31 @@
-console.log("Hallo")
+let input1 = [
+  [1, 3],
+  [2, 4],
+  [4, 5],
+  [8, 10],
+  [6, 13],
+];
+
+function merge(intervals) {
+  if (intervals.length < 2) {
+    return intervals;
+  }
+
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let result = [intervals[0]];
+
+  for (const interval of intervals) {
+    let recent = result[result.length - 1];
+
+    if (recent[1] >= interval[0]) {
+      recent[1] = Math.max(recent[1], interval[1]);
+    } else {
+      result.push(interval);
+    }
+  }
+
+  return result;
+}
+
+console.log(merge(input1));
